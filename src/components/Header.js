d71aed60448421903338ec37e99083ecf21ca466
Weiss Bai <whitesgr03@gmail.com>
2023-05-22 13:57:08 +0800

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = ({ state }) => (
+	<div className="header">
+		<h1>Zodiac Memory</h1>
+		<div className="scoreBoard">
+			<h3>Score: {state.score}</h3>
+			<h3>Best Score: {state.BestScore}</h3>
+		</div>
+	</div>
+);
+
+// const Navbar = () => (
+// 	<>
+
+// 	</>
+// );
+
+export default Header;
