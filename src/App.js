82ab8b06f5eac68a1de9c0c0272b200e05233fe5
Weiss Bai <whitesgr03@gmail.com>
2023-05-22 13:59:35 +0800

    Add initialData

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import Content from "./components/Content";
 import Footer from "./components/Footer";
 
 const App = () => {
-	return <div></div>;
+	const initialData = {
+		score: 0,
+		BastScore: 0,
+	};
+
+	const [state, setState] = useState(initialData);
+
 };
 
 export default App;
