fcbbe653067ec9b9dfdb2d72d11098a5711ec6b5
Weiss Bai <whitesgr03@gmail.com>
2023-05-23 09:54:20 +0800

    Refactor content component

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,21 +5,3 @@
 		})),
 		clickedSign: [],
 	};
-
-// const ListItems = () => {
-// 	const images = require.context("../image/zodiac", false, /.jpg$/);
-
-// 	const List = images.keys().map(key => {
-// 		const name = key.match(/(?<=\.\/)[^.]+/g)[0];
-// 		return (
-// 			<li className="item" key={name}>
-// 				<img src={images(key)} alt={name} />
-// 				<p>{name}</p>
-// 			</li>
-// 		);
-// 	});
-
-// 	return <ul>{List}</ul>;
-// };
-
-export default Content;
