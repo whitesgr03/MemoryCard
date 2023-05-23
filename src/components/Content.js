bc9c6abeeeb7355fdba46fd367a0202dc330f79c
Weiss Bai <whitesgr03@gmail.com>
2023-05-23 09:54:06 +0800

    Refactor content component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,25 +1,10 @@
-// import { useMemo, useCallback, useEffect } from "react";
-
-const Content = ({ onAddScore, onReSetScore }) => {
-	const images = require.context("../image/zodiac", false, /.jpg$/);
-
-	const List = images.keys().map(key => {
-		const name = key.match(/(?<=\.\/)[^.]+/g)[0];
-		return (
-			<li key={name}>
-				<button type="button">
-					<img src={images(key)} alt={name} />
-					<p>{name}</p>
-				</button>
-			</li>
-		);
-	});
-	return (
-		<div className="content">
-			<ul>{List}</ul>
-		</div>
-	);
-};
+	const initialData = {
+		listItems: images.keys().map(key => ({
+			name: key.match(/(?<=\.\/)[^.]+/g)[0],
+			url: images(key),
+		})),
+		clickedSign: [],
+	};
 
 // const ListItems = () => {
 // 	const images = require.context("../image/zodiac", false, /.jpg$/);
