120713aa41328f90b6ffcd0c084648b2b95a62d6
Weiss Bai <whitesgr03@gmail.com>
2023-05-24 12:35:36 +0800

    Add show info button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
-const Header = ({ state }) => (
+import Icon from "@mdi/react";
+import { mdiInformationSlabCircle } from "@mdi/js";
+
+const Header = ({ state, onShowInfo }) => (
 	<div className="header">
 		<h1>Zodiac Memory</h1>
 		<div className="scoreBoard">
-			<h3>Score: {state.score}</h3>
-			<h3>Best Score: {state.BestScore}</h3>
+			<button type="button">
+				<Icon
+					path={mdiInformationSlabCircle}
+					title="User Profile"
+					onClick={() => onShowInfo(true)}
+				/>
+			</button>
+			<div>
+				<h3>Score: {state.score}</h3>
+				<h3>Best Score: {state.BestScore}</h3>
+			</div>
 		</div>
 	</div>
 );
