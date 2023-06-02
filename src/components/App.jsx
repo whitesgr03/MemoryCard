3639b5cf7e6f420fd5201f86fdea728bccac0014
Weiss Bai <whitesgr03@gmail.com>
2023-06-02 13:11:30 +0800

    Update extension to jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 
 const App = () => {
 	const initialData = {
